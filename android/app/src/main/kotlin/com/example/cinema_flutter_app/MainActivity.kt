1441251ba971818a2143219ca3b4cb7d67a61f6e
mo <mohamedkx77@gmail.com>
2021-03-20 18:53:22 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cinema_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cinema_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cinema_flutter_app/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(CinemaApp());
+}
+
+class CinemaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      allowFontScaling: false,
+      builder: () => MaterialApp(
+        title: 'Cinema App',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
